ec893a1b5a4ec1e0a7cb61f0df758bccb32efbbf
Prissilla Escobar <prissilla988@gmail.com>
2024-01-02 01:22:10 -0700

    Create article detail page. Conditionally render the dropdown button.

--- src/Components/AllArticles/AllArticles.js
+++ src/Components/AllArticles/AllArticles.js
@@ -1,21 +1,31 @@
 import './AllArticles.css'
 import ArticleCard from '../ArticleCard/ArticleCard'
+import { Link } from 'react-router-dom'
 
+function AllArticles({ topArticles }) {
 
-function AllArticles({ topArticles, setServerError }) {
+  if (topArticles.length === 0) {
+    return (
+      <div className="no-articles-message">
+        <p>Select a country in the top right to see top articles!</p>
+      </div>
+    )
+  }
 
     const articleCards = topArticles.map(article => {
         return (
-          <ArticleCard 
-            title={article.title}
-            urlToImage={article.urlToImage}
-            publishedAt={article.publishedAt}
-            content={article.content}
-            description={article.description}
-            author={article.author}
-            url={article.url}
-            source={article.source.name}
-          />
+          <Link to={`/${article.title}`} className='card-link'>
+            <ArticleCard 
+              title={article.title}
+              urlToImage={article.urlToImage}
+              publishedAt={article.publishedAt}
+              content={article.content}
+              description={article.description}
+              author={article.author}
+              url={article.url}
+              source={article.source.name}
+            />
+          </Link>
         )
     })
 
--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,35 +1,9 @@
-export async function getAllArticles() {
-    let Url = 'https://newsapi.org/v2/everything?q=bitcoin&apiKey=adce009eed63469b8050f78512e9a7bc'
-    try {
-        const response = await fetch(Url)
-        if (!response.ok) {
-            throw new Error(`Articles not found.`)
-        }
-        return response.json()
-    } catch (error) {
-        throw new Error(`Error fetching albums: ${error.message}`)
-    }
-}
-
 export async function getTopArticles(country) {
     let Url = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=adce009eed63469b8050f78512e9a7bc`
     try {
         const response = await fetch(Url)
         if (!response.ok) {
             throw new Error(`Top articles not found.`)
-        }
-        return response.json()
-    } catch (error) {
-        throw new Error(`Error fetching albums: ${error.message}`)
-    }
-}
-
-export async function getSources() {
-    let Url = 'https://newsapi.org/v2/top-headlines/sources?apiKey=adce009eed63469b8050f78512e9a7bc'
-    try {
-        const response = await fetch(Url)
-        if (!response.ok) {
-            throw new Error(`Source not found.`)
         }
         return response.json()
     } catch (error) {
